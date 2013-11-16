9a153a0171c8dca5b41a09b29c5e50fa3c43a77b
Mike Knepper <michaelrknepper@gmail.com>
2013-11-16 15:50:29 -0600

    Initial validations and specs for Game. Building game board

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,17 @@
 class Game < ActiveRecord::Base
-  has_many :positions
+  has_many :spots
+
+  validates :status, presence: true
+
+  after_create :build_game_board
+
+
+  private
+
+  def build_game_board
+    9.times do |i|
+      Spot.create(game: self, position: i+1)
+    end
+  end
 
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe Game do
-  it 'has a status'
+  it 'has a valid factory' do
+    expect(build(:game)).to be_valid
+  end
 
-  it 'has nine spots'
+  it 'is invalid without a status' do
+    expect(build(:game, status: nil)).to_not be_valid
+  end
+
+  it 'has nine spots' do
+    game = create(:game)
+    expect(game.spots.length).to eq(9)
+  end
 
 end
