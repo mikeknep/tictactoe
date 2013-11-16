d09635020f2ad8199a88edc8ef1496c43cd59d86
Mike Knepper <michaelrknepper@gmail.com>
2013-11-16 14:58:00 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictactoe::Application.config.secret_key_base = '6ae2886728b49ba481504c9fa31d6356438a6fedcc1821c771bb7d526dc3a3e7d6debaf9e06aae88a7ce723a0b74a9130b4604eb3aa6b4cbfbc86e00bfb7f1c1'
